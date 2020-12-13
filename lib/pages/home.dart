966f7f5724dddc77acfe91cf51886fea1a461231
si67 <sonuishaq67@gmail.com>
2020-12-13 09:28:26 +0530

    buildkit=1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,7 +19,7 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.black,
         body: Center(
           child: Text(
-            '$osUsed',
+            'you use $osUsed',
             style: textStyleHeading,
           ),
         ),
