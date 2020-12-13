e5c7b938865a9bb5a6e465a384abf064e42ddddf
Luke Grant <ishaqshaik084@gmail.com>
2020-12-13 12:24:55 +0530

    paths are ambigious

--- login.sh
+++ login.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+pwd
 apt-get update
 apt-get install -y curl git wget unzip libgconf-2-4 gdb libstdc++6 libglu1-mesa fonts-droid-fallback lib32stdc++6 python3
 touch /root/.netrc
@@ -16,11 +16,11 @@ git config --global user.email "ishaqshaik084@gmail.com"
 echo "machine github.com" >>/root/.netrc
 echo "login $github_username" >>/root/.netrc
 echo "password $github_password" >>/root/.netrc
-git clone https://github.com/sonuishaq67/sonuishaq67.github.io sonuishaq67/sonuishaq67.github.io
-cd sonuishaq67
-cd sonuishaq67.github.io && sudo rm -r * && cd ../website_src
+git clone https://github.com/sonuishaq67/sonuishaq67.github.io 
+cd sonuishaq67.github.io && rm -r *
+cd ../website_src
 flutter clean
-flutter build web -v
+flutter build web 
 commitMsg=$(git log -1 | tail -n 1)
 cp -r build/web/* ../sonuishaq67.github.io/
-sudo cd sonuishaq67.github.io && git add . && git commit -m $commitMsg && git push
+cd sonuishaq67.github.io && git add . && git commit -m $commitMsg && git push
